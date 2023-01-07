5e2af6650b4f1885bf1591790b940e0a9318c2c1
s11fargo <122173639+s11fargo@users.noreply.github.com>
2023-01-07 10:46:48 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,14 +10,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <body>
+      <main>
         <Header title="Fargo Software" />
         <p className="details">
           Call (818) 370-xxxx 
         </p>
-  
-  <img src="Software.jpg" alt="software image" width="300" height="200">
-      </body>
+      </main>
 
 
       <Footer />
