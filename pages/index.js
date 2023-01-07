73e843060cc7ab5203a9c466016edf1d54134e2f
s11fargo <122173639+s11fargo@users.noreply.github.com>
2023-01-07 11:03:34 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,9 +16,10 @@ export default function Home() {
           Call (818) 370-xxxx 
         </p>
       </main>
-<body>
-    
-    </body>
+  
+      <body>
+        <img src="Software.jpg" alt="Software Image" width="300" height="200">
+      </body>
 
       <Footer />
     </div>
