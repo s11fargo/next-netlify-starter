f3fb4d93189a88a193f52690cc04020bc3242b94
s11fargo <122173639+s11fargo@users.noreply.github.com>
2023-01-09 13:52:10 -0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -5,10 +5,10 @@ export default function Footer() {
     <>
     <footer className={styles.footer}>
         <ul class="mylinks">
-             <li><a href="https://fargosoftware.netlify.app/">Made by Fargo Software</a></li>
-             <li><a href="https://www.linkedin.com/in/sharon-fargo-a71b1054//">Linked In</a></li>
-             <li><a href="https://www.instagram.com/">Instagram</a></li>
-             <li><a href="https://www.yelp.com/">Yelp</a></li>
+             <li><a href="https://fargosoftware.netlify.app/" target="_blank">Made by Fargo Software</a></li>
+             <li><a href="https://www.linkedin.com/in/sharon-fargo-a71b1054//" target="_blank">Linked In</a></li>
+             <li><a href="https://www.instagram.com/" target="_blank">Instagram</a></li>
+             <li><a href="https://www.yelp.com/" target="_blank">Yelp</a></li>
         </ul>      
     </footer>
     </>
