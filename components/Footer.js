ffb633ceb9c60a25bf027526934556e728b6e5a9
s11fargo <122173639+s11fargo@users.noreply.github.com>
2025-02-23 19:33:21 -0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
         <ul class="mylinks">
              <li><a href="https://www.linkedin.com/in/sharon-fargo-a71b1054/" target="_blank">Linked In</a></li>
              <li><a href="https://www.yelp.com/" target="_blank">Yelp</a></li>
-             <li><a href="https://fargonotary.netlify.app/" target="_blank">Fargo Notary</a></li>
+             <li><a href="https://fargonotary.netlify.app" target="_blank">Fargo Notary</a></li>
         </ul>      
     </footer>
     </>
